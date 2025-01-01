3ec83603f7d12425884656b053436bade2f3214d
demmynile <balogun80@gmail.com>
2025-01-01 04:24:35 +0000

    added new files

--- incomeexpensesapi/incomeexpensesapi/settings.py
+++ incomeexpensesapi/incomeexpensesapi/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -134,9 +136,10 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-
+EMAIL_BACKEND = 'authentication.OAuth2EmailBackend.OAuth2EmailBackend'
 EMAIL_USE_TLS=True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_HOST_USER= os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD= os.environ.get('EMAIL_HOST_PASSWORD')
+TOKEN_FILE = BASE_DIR / "token_file.json"
+
