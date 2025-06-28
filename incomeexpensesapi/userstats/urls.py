6605ccd9de55babe2e99454cbdb765490506177a
demmynile <balogun80@gmail.com>
2025-06-28 15:54:21 +0100

    add new features

--- incomeexpensesapi/userstats/urls.py
+++ incomeexpensesapi/userstats/urls.py
@@ -1,6 +1,7 @@
-from .views import ExpenseSummaryStats
+from .views import ExpenseSummaryStats , IncomeSourcesSummaryStats
 from django.urls import path
 
 urlpatterns = [
     path('expense_category_data/', ExpenseSummaryStats.as_view(), name='expense_category_summary'),
+    path('income_sources_data/', IncomeSourcesSummaryStats.as_view(), name='income_sources_data')
 ]
